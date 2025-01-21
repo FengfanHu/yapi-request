49429a77615ec5fae2b6eb952ee9053447f29552
蜂窝 <hufengfan@cai-inc.com>
2025-01-21 10:49:45 +0800

    feat: [initialization] setup extension and webview

---
+++ packages/yapi-request/src/view-provider/view-provider-sidebar.ts
@@ -0,0 +1,23 @@
+import { ExtensionContext, WebviewView, WebviewViewProvider } from "vscode";
+import { AbstractViewProvider } from "./view-provider-abstract";
+
+export class ViewProviderSidebar
+  extends AbstractViewProvider
+  implements WebviewViewProvider
+{
+  constructor(context: ExtensionContext) {
+    super(context, {
+      distDir: "out/webview",
+      indexPath: "out/webview/index.html",
+    });
+  }
+
+  async resolveWebviewView(webviewView: WebviewView) {
+    const { webview } = webviewView;
+    webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this.context.extensionUri],
+    };
+    webview.html = await this.getWebviewHtml(webview);
+  }
+}
